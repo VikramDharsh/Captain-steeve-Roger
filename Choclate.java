bbbc23471f414d91ef30504fc1acb849c8663297
VikramDharsh <113521905+VikramDharsh@users.noreply.github.com>
2022-10-11 13:52:22 +0530

    main

---
+++ Choclate.java
@@ -0,0 +1,11 @@
+package com.xworkz.learningConstructor;
+
+public class Choclate {
+	public String name;
+	public String brand;
+	public int price;
+	public String flavour;
+	 public Choclate() // explicit // default //no argument
+	{}
+
+}
---
+++ DistrictCollectorRunner.java
@@ -0,0 +1,27 @@
+package com.xworkz.learningConstructor;
+import  com.xworkz.learningConstructor.*;
+
+public class DistrictCollectorRunner {
+	
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		// TODO Auto-generated method stub
+		 DistrictCollector dc=	new  DistrictCollector (); 
+		 dc.name="late DK RAVI sir";
+		 dc.batchNo=2009;
+		 dc.age=34;
+		 dc.district="kolar";
+		 
+		 System.out.println("name :"+dc.name);
+		 System.out.println("age :"+dc.age);
+		 System.out.println("district  :"+dc.district);
+		 System.out.println("batch no  :"+dc.batchNo);
+		 
+		 System.out.println("end");
+		
+		
+
+	}
+
+}
