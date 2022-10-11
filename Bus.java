d58dfc3f2dfc9aef7a3f638d979223cdc280ad1c
VikramDharsh <113521905+VikramDharsh@users.noreply.github.com>
2022-10-11 19:04:14 +0530

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,14 @@
+package com.xworkz.learningConstructor;
+
+public class Bus {
+	public int num;
+	public String startPlace;
+	public String destination;
+	public Bus()
+	
+	{
+		// explicit  
+		System.out.println("constructor working");
+	}
+
+}
